49586f4c66a529fa8ba963b2c79fd3796de1d7c6
Chris Shucksmith <chris@shucksmith.co.uk>
2017-03-30 20:36:13 +0100

    support groups

--- phonebook/admin.py
+++ phonebook/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import Contact
+from .models import Contact, Group
 import datetime
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'phone_number')
+    list_display = ('first_name', 'last_name', 'phone_number', 'job_title')
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'index')
 
 admin.site.register(Contact, ContactAdmin)
+admin.site.register(Group, GroupAdmin)
--- phonebook/models.py
+++ phonebook/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+class Group(models.Model):
+	name = models.CharField(max_length=10)
+	index = models.PositiveSmallIntegerField(unique=True)
+
+    def __str__(self):
+        return "{0}".format(self.name)
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=100, blank=True)
+    job_title = models.CharField(max_length=100, blank=True)
+    group = models.ForeignKey(Group, blank=True, null=True)
 
     def __str__(self):
         return "{0} {1}".format(self.first_name, self.last_name)
